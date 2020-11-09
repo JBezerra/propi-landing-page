80e89e8fa38bc48ce59c82d1fd50d466f703899a
José Bezerra <jbmn2@cin.ufpe.br>
2020-11-09 19:38:01 -0300

    feat(first-page): first page implemented

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,31 @@
 import Head from 'next/head'
-import styles from '../styles/LandingPage.module.css'
+import styles from '../styles/Landing.module.css'
 
-export default function LandingPage() {
+export default function Landing() {
   return (
-    <div id={styles.landingPageContainer} className='container'>
+    <div id={styles.landingContainer} className='container'>
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet"></link>
       </Head>
+
       <div className={styles.topContainer}>
-        <h1>Hello World!</h1>
+        <img src='/assets/images/logo.svg' alt='Propi' />
+      </div>
+
+      <div className={styles.mainContent}>
+
+        <div className={styles.mainTextContainer}>
+          <div className={styles.titleContainer}>
+            <h1>Burocracia de imóveis Simplificada</h1>
+            <p>Resolva toda a burocracia documental de imóveis online, em poucos minutos e sem sair de casa.</p>
+          </div>
+          <button>Começar</button>
+        </div>
+
+        <div className={styles.heroContainer}>
+          <img src='/assets/images/hero.svg' alt='Propi' />
+        </div>
+
       </div>
     </div>
 
